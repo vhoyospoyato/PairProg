cb241d0c881b2b21dad31585f746c3db3067af0a
JulianT <jtannus.ieu2017@student.ie.edu>
2019-10-26 18:57:14 +0200

    Second Commit

--- src/Main.java
+++ src/Main.java
@@ -9,39 +9,32 @@ public class Main {
         BufferedReader br = new BufferedReader(new FileReader(file));
         BufferedWriter bw = new BufferedWriter(new FileWriter("sortededdict.txt"));
 
-        LinkedList<String> dictionary = new LinkedList<String>();
+        LinkedList<Node> dictionary = new LinkedList<Node>();
         String line;
         String tempLine = "";
-        int index = 0;
-
+        Node head = null;
 
         while ((line = br.readLine()) != null) {
-            dictionary.add(line);
-            if (index > 1) {
-                int test = line.compareToIgnoreCase(tempLine);
-                if (test < 0) {
-                    dictionary.remove(line);
-                    dictionary.add(dictionary.indexOf(tempLine), line);
-                    System.out.println(dictionary);
-                }
-                else {
-                    tempLine = line;
-                }
-            }
-            else if (index == 1){
-                int test = line.compareToIgnoreCase(tempLine);
-                if (test < 0) {
-                    dictionary.remove(line);
-                    dictionary.addFirst(line);
-                    System.out.println(dictionary);
-                }
+            if (head == null)
+            {
+                head = new Node(line);
+                dictionary.add(head);
+                start = false;
             }
             else{
-                tempLine = line;
+                Node current = head;
+                Node previous;
+                    while(current.next != null){
+                        if (current.data.compareToIgnoreCase(head.data) < 0){
+                            head.next = current;
+                            previous = current;
+                        }
+                        current = current.next;
+                    }
             }
             bw.write(line);
             bw.newLine();
-            index++;
         }
+        System.out.println(dictionary.get(0).data);
     }
 }
---
+++ src/Node.java
@@ -0,0 +1,10 @@
+public class Node {
+
+    String data;
+    Node next;
+
+    Node(String data) {
+        this.data = data;
+        next = null;
+    }
+}
