064550e71206284c2bdc64ca6380683e8b4286d6
Vichy <vhoyos.ieu2017@student.ie.edu>
2019-11-10 23:53:23 +0100

    Time average

--- src/Main.java
+++ src/Main.java
@@ -110,7 +110,9 @@ public class Main {
             }
         }
 
-
+        System.out.print("Rest nodes added time taken: ");
+        System.out.print(System.currentTimeMillis() - start);   //Prints total sorting time
+        System.out.println(" milliseconds.");
 
         br.close();
 
@@ -118,12 +120,86 @@ public class Main {
             bw.write(dictionary.get(i).data);
             bw.newLine();
         }
-
         bw.close();
-        System.out.print("Rest nodes added time taken: ");
-        System.out.print(System.currentTimeMillis() - start);   //Prints total sorting time
-        System.out.println(" milliseconds.");
+
+        performance();
 
         userArguments(dictionary);                              //Execute method for user to enter arguments
+    }
+
+    public static void performance() throws IOException {
+
+        long time = 0;
+
+        for (int i = 0; i < 10; i++) {
+
+            long start = System.currentTimeMillis();                                            //Timer starts.
+            File file = new File("unsortedDictTest.txt");
+            BufferedReader br = new BufferedReader(new FileReader(file));
+            BufferedWriter bw = new BufferedWriter(new FileWriter("sortededdict.txt"));
+
+            LinkedList<Node> dictionary = new LinkedList<Node>();
+            Node head = null;
+            String line;
+
+            while ((line = br.readLine()) != null) {
+                if (dictionary.isEmpty()) {
+                    head = new Node(line);
+                    dictionary.add(head);                           //First node inserted in dictionary
+                }
+
+                else {
+                    head = dictionary.getFirst();
+                    if (head.next == null) {                        //Checks if there is a second value in dictionary
+                        if (head.data.compareToIgnoreCase(line) > 0) {
+                            Node temp = head;
+                            head = new Node(line);
+                            head.next = temp;
+                            dictionary.addFirst(head);              //Enters new node as first and pushes other to second
+                        }
+                        else {
+                            Node temp = new Node(line);
+                            head.next = temp;
+                            dictionary.add(temp);                   //Enters new node as second
+                        }
+                    }
+
+                    else {
+                        Node current = head;
+                        int index = 0;
+                        while (current.next != null){
+                            if (current.data.compareToIgnoreCase(line) > 0) { //Check if word comes before in alphabet
+                                Node temp = new Node(line);
+                                temp.next = current;
+                                if (index > 0){                               //For every value, except first
+                                    dictionary.get(index - 1).next = temp;
+                                }
+                                dictionary.add(index, temp);                  //Add node to correct alphabetical position
+                                break;
+                            }
+                            else{
+                                current = current.next;             //If words comes later in the alphabet move to next node
+                                if (current.next == null){
+                                    Node temp = new Node(line);
+                                    dictionary.getLast().next = temp;
+                                    dictionary.addLast(temp);       //Add node in last position
+                                    break;
+                                }
+                                index ++;
+                            }
+                        }
+
+                    }
+
+                }
+
+            }
+            long end = System.currentTimeMillis();
+            long test = end - start;
+            time = time + test;
+        }
+
+        System.out.println("Average time: " + (time/10));
+
     }
 }
